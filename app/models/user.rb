8d79fae60e50f851ec84d7e8742f5012a14894ca
komatsushota <komatsushota@yahoo.com>
2015-11-07 21:37:32 +0900

    First

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable, :timeoutable, :omniauthable,omniauth_providers: [:facebook]
+  # omniauth_providers: [:twitter],
+
+  def self.from_omniauth(auth)
+    where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.email = auth["info"]["email"]
+      user.password = Devise.friendly_token[0,20]
+      if auth["provider"] == "facebook"
+        user.username = auth["info"]["name"]
+      elsif auth["provider"] == "twitter"
+        user.username = auth["info"]["nickname"]
+      end
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+end
